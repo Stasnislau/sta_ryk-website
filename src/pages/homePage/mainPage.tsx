7c7a7cdd364055f75a791533290d59a4b959f2b7
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-10-29 14:33:31 +0100

    initial skeleton

--- src/pages/homePage/mainPage.tsx
+++ src/pages/homePage/mainPage.tsx
@@ -1,8 +1,21 @@
 
 const MainPage = () => {
     return (
-        <div className="text-10xl bg-red-950">
-            main page
+        <div className="w-full overscroll-container flex flex-col">
+            <div className="w-full h-screen flex-grow-1 ">
+                About me section
+            </div>
+            <div className="w-full h-screen flex-grow-1">
+                experience section
+            </div>
+            <div className="w-full h-screen flex-grow-1">
+                Projects section
+            </div>
+
+            <div className="w-full h-screen flex-grow-1">
+                Contact section
+            </div>
+
         </div>
     );
 }
