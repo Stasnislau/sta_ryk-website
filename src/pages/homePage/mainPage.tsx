c56f991c55efeb8e3cd6d6ba3170ba27ff75b595
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-10-29 19:57:41 +0100

    Add scroll effect and section containers to home page

--- src/pages/homePage/mainPage.tsx
+++ src/pages/homePage/mainPage.tsx
@@ -1,20 +1,35 @@
 
+import { useEffect } from 'react';
+import './index.scss';
 const MainPage = () => {
+    const setScrollValue = () => {
+        const htmlElement = document.documentElement;
+        const percentOfScreenHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight;
+        console.log(Math.min(percentOfScreenHeightScrolled * 100, 100))
+        htmlElement.style.setProperty('--scroll', Math.min(percentOfScreenHeightScrolled * 100, 100).toString());
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', setScrollValue);
+        window.addEventListener('resize', setScrollValue);
+    }, [])
     return (
-        <div className="w-full overscroll-container flex flex-col">
-            <div className="w-full h-screen flex-grow-1 ">
-                About me section
-            </div>
-            <div className="w-full h-screen flex-grow-1">
+        <div className="w-full flex flex-col sections-container ">
+            <section className="w-full h-screen flex-grow-1 top-0 top-section sticky">
+                <div className="w-full h-full flex flex-row items-center">
+                    <div className='w-6/12 h-full left'> About me section </div>
+                    <div className='w-6/12 h-full right'> reference</div>
+                </div>
+            </section>
+            <section className="w-full h-screen flex-grow-1 bg-yellow-50 text-gray-600 mt-100">
                 experience section
-            </div>
-            <div className="w-full h-screen flex-grow-1">
+            </section>
+            <section className="w-full h-screen flex-grow-1">
                 Projects section
-            </div>
+            </section>
 
-            <div className="w-full h-screen flex-grow-1">
+            <section className="w-full h-screen flex-grow-1">
                 Contact section
-            </div>
+            </section>
 
         </div>
     );
