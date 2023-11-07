6a62206e17e660653db56344320e45186ab908a1
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-11-07 16:41:04 +0100

    Update title and add Prettier and TailwindCSS plugins

--- src/pages/homePage/mainPage.tsx
+++ src/pages/homePage/mainPage.tsx
@@ -1,8 +1,12 @@
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import './index.scss';
 import Header from '../../components/header/header';
 const MainPage = () => {
+    const aboutRef = useRef<HTMLDivElement>(null);
+    const experienceRef = useRef<HTMLDivElement>(null);
+    const projectsRef = useRef<HTMLDivElement>(null);
+    const contactRef = useRef<HTMLDivElement>(null);
     const setScrollValue = () => {
         const htmlElement = document.documentElement;
         const percentOfScreenHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight;
@@ -15,22 +19,27 @@ const MainPage = () => {
     return (
         <div className="w-full flex flex-col sections-container ">
             <div className="w-full h-20 flex flex-row">
-                <Header />
+                <Header 
+                    aboutRef={aboutRef}
+                    experienceRef={experienceRef}
+                    projectsRef={projectsRef}
+                    contactRef={contactRef}
+                />
             </div>
-            <section className="w-full h-screen flex-grow-1 top-0 top-section sticky">
+            <section className="w-full h-screen flex-grow-1 top-0 top-section sticky" ref={aboutRef}>
                 <div className="w-full h-full flex flex-row items-center">
                     <div className='w-6/12 h-full left'/>
                     <div className='w-6/12 h-full right'> reference</div>
                 </div>
             </section>
-            <section className="w-full h-screen flex-grow-1 mt-100 first-section">
+            <section className="w-full h-screen flex-grow-1 mt-100 first-section" ref={experienceRef}>
                 experience section
             </section>
-            <section className="w-full h-screen flex-grow-1">
+            <section className="w-full h-screen flex-grow-1" ref={projectsRef}>
                 Projects section
             </section>
 
-            <section className="w-full h-screen flex-grow-1">
+            <section className="w-full h-screen flex-grow-1" ref={contactRef}>
                 Contact section
             </section>
 
