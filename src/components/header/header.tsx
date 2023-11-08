d54134fd3a7e8cfa0351dc175558ba156e8eab67
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-11-08 18:09:00 +0100

    Update icons and styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,17 +12,17 @@ const Header = (
 ) => { // add references to all the sections
     return (
         <header className="flex flex-grow-1 w-full flex-row justify-between align-center">
-            <div className="flex flex-row icons items-center w-3/12 gap-5">
+            <div className="flex flex-row icons items-center w-3/12 gap-2">
                 <a href="
                 https://www.linkedin.com/in/staryk/">
-                    <img src="/icons/linked-in.svg" alt="linkedin" className='h-10' />
+                    <img src="/icons/linkedin.svg" alt="linkedin" className='h-10' />
                 </a>
                 <a href="https://github.com/Stasnislau">
                     <img src="/icons/github.svg" alt="github" className='h-10' />
                 </a>
-                
+
             </div>
-            <div className="flex flex-row buttons flex-grow justify-between">
+            <div className="flex flex-row buttons flex-grow justify-between ">
                 <button onClick={() => aboutRef?.current?.scrollIntoView({ behavior: 'smooth' })}>About</button>
                 <button onClick={() => experienceRef?.current?.scrollIntoView({ behavior: 'smooth' })}>Experience</button>
                 <button onClick={() => projectsRef?.current?.scrollIntoView({ behavior: 'smooth' })}>Projects</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      mobile: "375px",
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1440px",
+    },
     extend: {
       colors: {
         text: "#DFE2E7",
