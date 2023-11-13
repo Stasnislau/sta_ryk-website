72a0f23b2b11d46297daaa9a0368720131e93008
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-11-13 22:35:39 +0100

    added skeletons of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
+import { BrowserRouter } from "react-router-dom"
+import { Navbar, Hero } from "./components"
 
 function App() {
 
   return (
-    <div>
-      My personal website
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import Navbar from "./navbar";
+import Hero from "./hero";
+import about from "./about";
+import Experience from "./experience";
+import Feedbacks from "./feedbacks";
+import tech from "./tech";
+import Works from "./works";
+
+export { Navbar, Hero, about, Experience, Feedbacks, tech, Works };
