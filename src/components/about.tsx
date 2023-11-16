f98d12154c1aa81ee2a0fdf6a0ea6cf081404d76
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-11-17 00:15:59 +0100

    Refactor about page to use Tilt component for service cards

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,11 +1,36 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
 
 import { fadeIn, textVariant } from "../utils/motion";
+
+interface ServiceCardProps {
+    title: string;
+    index: number;
+    icon: string;
+}
+
+const ServiceCard = ({ index, title, icon }: ServiceCardProps) => {
+    return (
+        <Tilt className="xs:w-[250px] w-full" options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+        }}>
+            <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+                <div
+                    className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+                >
+                    <img src={icon} alt="icon" className="w-16 h-16 object-contain" />
+                    <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+                </div>
+            </motion.div>
+        </Tilt >
+    );
+}
 
 const About = () => {
     return (
@@ -17,6 +42,13 @@ const About = () => {
             <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]" >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo deleniti sequi necessitatibus tenetur voluptatem rem, possimus ratione odio libero similique obcaecati dicta neque. Quisquam ex iste blanditiis quam possimus. Id!
             </motion.p>
+            <div className="mt-20 flex flex-wrap gap-10">
+                {
+                    services.map((service, index) => (
+                        <ServiceCard key={service.title} index={index} title={service.title} icon={service.icon} />
+                    ))
+                }
+            </div>
         </>
     );
 }
