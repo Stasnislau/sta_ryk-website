11f0cae484f1497d34dddc2075b6d5b31c679e16
Stanislau Ryzhkou <stanislaurv@gmail.com>
2024-01-14 12:49:23 +0100

    improved icons

--- src/components/languages.tsx
+++ src/components/languages.tsx
@@ -16,11 +16,11 @@ const LanguageCard = ({ language, index }:
     }) => {
     return (
         <motion.div variants={fadeIn("up", "string", index * 0.5, 0.75)} custom={index}>
-            <div className="flex flex-col items-center justify-center text-center">
+            <div className="flex flex-col items-center justify-center text-center bg-tertiary rounded-[20px] shadow-card p-4">
                 <p className="text-white text-[14px] mb-2">{language.name}</p>
                 <div className="relative w-32 h-32 p-4">
                     <div className="relative w-full h-full rounded-full bg-tertiary opacity-100">
-                        <div className="absolute top-0 left-0 w-full h-full border-[10px] rounded-full bg-tertiary"></div>
+                        <div className="absolute top-0 left-0 w-full h-full border-[10px] border-[#AAA6c3] rounded-full bg-tertiary"></div>
                         <div className="absolute top-0 left-0 w-full h-full rounded-full items-center align-center justify-center flex flex-col"
                             style={{
                                 background: `conic-gradient(#915eff 0% ${language.progress}%, transparent 0% 100%)`,
